7f686c954bbcc946b7864445557f15e77cbe9f8b
The-Lup <joanantonio95@gmail.com>
2025-11-09 12:54:08 -0400

    Code update v1.0.1

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hello world!!!!');
+const express = require('express');
+require('dotenv').config();
+
+//create express server
+const app = express();
+
+//Public dir
+app.use(express.static('public'));
+
+//Routes
+app.use('/api/auth', require('./routes/auth'));
+//TODO: auth// create, login, renew
+//TODO: CRUD: events
+
+//Listen petitions
+app.listen(process.env.PORT, () => {
+  console.log(`Server running on port ${process.env.PORT}`);
+});
